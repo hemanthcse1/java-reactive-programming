15182f135d461d98b133a8e8db284510f9dcf34e
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-09 19:46:59 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/javareactiveaprogramaming/JavaReactiveaProgramamingApplication.java
@@ -0,0 +1,14 @@
+package com.hemanth.javareactiveaprogramaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactiveaProgramamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaReactiveaProgramamingApplication.class, args);
+        System.out.println("rective progrmming");
+    }
+
+}
---
+++ src/test/java/com/hemanth/javareactiveaprogramaming/JavaReactiveaProgramamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.javareactiveaprogramaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactiveaProgramamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
